57cfe44627e4a558d269b46f494fc3a0a831ba55
Paud-weerasuriya <udaraweerasuriya2@gmail.com>
2022-11-21 20:33:13 +0530

    4th

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -17,8 +17,8 @@ function HomeScreen() {
         isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchurl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchürl={requests.fetchActionMovies} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
