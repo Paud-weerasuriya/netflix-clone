f1cda1d20479220f0c055e1d0306aa84f61936d3
Paud-weerasuriya <udaraweerasuriya2@gmail.com>
2022-11-21 10:51:00 +0530

    second commit

---
+++ src/Banner.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixoriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_content">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {truncate(
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint Sed q`,
+            150
+          )}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Go3EFCOVKrsO6uadn3dc7Aaw7lWtz7c",
+  authDomain: "netflix-clone-e1e27.firebaseapp.com",
+  projectId: "netflix-clone-e1e27",
+  storageBucket: "netflix-clone-e1e27.appspot.com",
+  messagingSenderId: "106497687895",
+  appId: "1:106497687895:web:5f90b659050a6263f67e12",
+};
